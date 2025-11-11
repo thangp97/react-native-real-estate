6bb9a03a88cf483976a1172154abc2bcde3745f0
thang <thangcom565@gmail.com>
2025-11-11 16:12:44 +0700

    details screen

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -33,7 +33,7 @@ export const FeaturedCard = ({item: {image, rating, name, address, price}, onPre
 
                 <View className={"flex flex-row items-center justify-between w-full"}>
                     <Text className={"text-xl font-rubik-extrabold text-white"}>
-                        {price}
+                        ${price}
                     </Text>
                     <Image source={icons.heart} className={"size-5"} />
                 </View>
@@ -65,7 +65,7 @@ export const Card = ({item: {image, rating, name, address, price}, onPress}: Pro
 
                 <View className={"flex flex-row items-center justify-between mt-2"}>
                     <Text className={"text-base font-rubik-bold text-primary-300"}>
-                        {price}
+                        ${price}
                     </Text>
                     <Image source={icons.heart} className={"w-5 h-5 mr-2 size-5"} tintColor={"#191d31"} />
                 </View>
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -146,3 +146,17 @@ export async function getProperties({filter, query, limit}:{
         return [];
     }
 }
+
+export async function getPropertyById({ id }: { id: string }) {
+    try {
+        const result = await databases.getDocument(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            id
+        );
+        return result;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
